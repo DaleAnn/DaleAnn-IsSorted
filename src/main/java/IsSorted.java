c7dcdf017b69500bfd01312f1220d02ed851f735
DaleAnn <daviddaleann@yahoo.com>
2023-03-07 20:49:27 +0000

    Done!

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,14 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > arr[i+1]){
+                return false;
+            } else if(arr[i] < arr[i+1]){
+                return true;
+            }
+            // This makes no sense to me, why was I forced to repeat the return command?
+        }
+        return true;
     }
 }
